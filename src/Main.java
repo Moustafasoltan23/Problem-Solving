f316c42d596a18b7364d1f84216c4572ba130f85
Moustafasoltan23 <144147696+Moustafasoltan23@users.noreply.github.com>
2024-10-02 18:59:25 +0300

    Problem Solving

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
-        Scanner inpput = new Scanner(System.in);
-        System.out.println("plese enter your first num");
-        int x = inpput.nextInt();
-        System.out.println("please enter your secound num");
-        int  y = inpput.nextInt();
-        if (x%y==0){
-            System.out.println("this number evaltionn ");
-
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num <= 2){
+            System.out.println("No ");
+            return;
+        }
+        if (num %2 ==0){
+            System.out.println("Yes");
 
         }else {
-            System.out.println("this number not eveltion");
+            System.out.println("No");
         }
 
 
